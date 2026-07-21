47abd4b870b07d792c2bbcb33a6ada3b0f5df804
Riya <riya.patwardhan@cumminscollege.in>
2026-07-21 23:41:55 +0530

    Add index.html

--- main.py
+++ main.py
@@ -7,30 +7,7 @@ conn=mysql.connector.connect(
     database='project'
 )
 
-print('hello')
-
-if conn.is_connected():
-    print("✅ Connected to MySQL successfully!")
-
 cursor = conn.cursor()
 
-query = """
-INSERT INTO Student(StudentID, Name, CGPA)
-VALUES (%s, %s, %s)
-"""
-
-values = (1, "Riya", 9.51)
-
-cursor.execute(query, values)
-
-conn.commit()
-
-print("1 record inserted.")
-
 cursor.execute("SELECT * FROM Student")
-
-for row in cursor.fetchall():
-    print(row)
-
-cursor.close()
-conn.close()
+print(cursor.fetchall())
