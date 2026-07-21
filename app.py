6cf7c825f040bbc3b9839ff7e20636a2f719b180
Riya <riya.patwardhan@cumminscollege.in>
2026-07-22 00:03:06 +0530

    add changes

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template 
-app = Flask(__name__) 
-@app.route("/") 
-def home(): 
-    return render_template("index.html")
-    
-if __name__ == "__main__": 
-    app.run(debug=True)
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    print("Request Method:", request.method)
+
+    question = ""
+    answer = ""
+
+    if request.method == "POST":
+
+        question = request.form["question"]
+
+        # For now, pretend this is the chatbot answer
+        answer = "You asked: " + question
+
+    return render_template(
+        "index.html",
+        question=question,
+        answer=answer
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
