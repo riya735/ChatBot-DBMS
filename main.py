71f1f03781096a461c1764da5b2b31e7be9118c1
Riya <riya.patwardhan@cumminscollege.in>
2026-07-25 00:57:08 +0530

    some change

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import mysql.connector
 conn=mysql.connector.connect(
     host='localhost',
     user='root',
-    password='root',
-    database='chatbot_db'
+    password='riyamysql2025!',
+    database='project'
 )
 
 cursor = conn.cursor()
-
-cursor.execute("SELECT * FROM Student")
-print(cursor.fetchall())
